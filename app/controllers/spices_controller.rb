b86d3d5e2f15b3a3a0b4d655b34715ecaed4804d
Andrew Pellum <apellum19@gmail.com>
2021-09-10 13:24:43 -0600

    commit tree f2932c0b97b3f2f7c211e32a972f5cf5ce86b262

---
+++ app/controllers/spices_controller.rb
@@ -0,0 +1,37 @@
+class SpicesController < ApplicationController
+
+    def index
+        spices = Spice.all
+        render json: spices
+    end
+
+    def create
+        spice = Spice.create(spice_params)
+        render json: spice, status: :created
+    end
+
+    def update
+        spice = find_by
+        spice.update(spice_params)
+        render json: spice
+    end
+
+    def destroy
+        spice = find_by
+        spice.destroy
+        head :no_content
+    end
+
+
+
+    private
+
+    def spice_params
+        params.permit(:title, :image, :description, :notes, :rating)
+    end
+
+    def find_by
+        Spice.find_by(id: params[:id])
+    end
+end
+
